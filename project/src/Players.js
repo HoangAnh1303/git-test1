3c488a290ff3697ed0715b44e5d0fb689f9f37bb
Hoang Anh <hoanganhnguyen@gmail.com>
2023-05-15 23:03:04 +0700

    first commit

---
+++ project/src/Players.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+export default class player extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='asserts/images/cr.jpg' />
+                        <h3>Cristiano Ronaldo</h3>
+                        <p className='title'>Manchester United</p>
+                        <p><button>Detail</button></p>
+                    </div>
+                </div>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='asserts/images/kante.jpg'></img>
+                        <h3>Kante</h3>
+                        <p className='title'>Chelsea</p>
+                        <p><button>Detail</button></p>
+                    </div>
+                </div>
+
+                <div className='column'>
+                    <div className='card'>
+                        <img src='asserts/images/messi.jpg'></img>
+                        <h3>Messi</h3>
+                        <p className='title'>PSG</p>
+                        <p><button>Detail</button></p>
+                    </div>
+                </div>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='asserts/images/neymar.jpg'></img>
+                        <h3>Neymar</h3>
+                        <p className='title'>PSG</p>
+                        <p><button>Detail</button></p>
+                    </div>
+                </div>
+
+                <div className='column'>
+                    <div className='card'>
+                        <img src='asserts/images/kane.jpg'></img>
+                        <h3>Kane</h3>
+                        <p className='title'>Tottemham</p>
+                        <p><button>Detail</button></p>
+                    </div>
+                </div>
+                <div className='column'>
+                    <div className='card'>
+                        <img src='asserts/images/messi.jpg'></img>
+                        <h3>Messi</h3>
+                        <p className='title'>PSG</p>
+                        <p><button>Detail</button></p>
+                    </div>
+                </div>
+            </div>
+
+
+
+)
+
+    }
+
+}
