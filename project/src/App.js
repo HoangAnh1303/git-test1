a1c2042308e7b5c9553bc5fa487375f7bb2b1e85
Hoang Anh <hoanganhnguyen@gmail.com>
2023-05-31 16:03:08 +0700

    a

--- project/src/App.js
+++ project/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navigation from './component/Navigation';
 // import Main from './component/Main';
 import Players from './Players';
+import Footers from './Footer';
 function App(){
     return(
 <div className='App'>
 <Navigation/>
 {/* <Main/> */}
 <Players/>
+<Footers />
 </div>
 
     );
