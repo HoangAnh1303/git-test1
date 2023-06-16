7843df065ac9e453fea4be3a499aa0205c0b621f
Hoang Anh <hoanganhnguyen@gmail.com>
2023-06-16 15:43:53 +0700

    ex12

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Navigation from './component/Navigation';
-import Main from './component/Main';
+import Players from './component/Players';
+import Detail from './component/Detail';
+import Footer from './Footer';
+import {
+    Routes,
+    Route,
+  } from "react-router-dom";
+  
 function App(){
     return(
-<div className='App'>
+<div >
 <Navigation/>
-<Main/>
+<Routes>
+          <Route path='/' element={<Players/>}> 
+      </Route>
+        <Route path='/detail/:id' element={<Detail/>}></Route>
+        {/* <Route path='/contact' element={<Contact/>}></Route> */}
+      </Routes>
+      <Footer/>
+    </div>
 
-</div>
+
+
 
     );
 }
